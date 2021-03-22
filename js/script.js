617d81c6ebb8c7322c646c01b7533bcd8cb53969
Aramex86 <slavbez@mail.ru>
2021-03-22 14:50:26 +0200

    media ipad

--- js/script.js
+++ js/script.js
@@ -89,9 +89,18 @@ const cupons = [
 
 const cardWrap = document.querySelector(".card__wrapp");
 const menuItems = document.querySelectorAll(".sidebar__list-item");
-const menuImg = document.querySelectorAll(".sidebar__list-item img");
 const headerName = document.querySelector(".header__wrapp-heading");
-const mobileBnt = document.querySelector('.mobile__menu-btn');
+const mobileBnt = document.querySelector(".mobile__menu-btn");
+const mobileBntClose = document.querySelector(".close");
+const sidebarMobile = document.querySelector(".sidebar__wrapp--mobile");
+const sideBarMobileOepen = document.querySelector(".sidebar__open");
+const displayMenuText = document.querySelectorAll(".mobile__text");
+const logoImg = document.querySelector(".sidebar__logo img");
+const inputWraper = document.querySelector('.header__wrapp-input');
+const headerWraper = document.querySelector('.header__wrapp');
+const bodyWrapp = document.querySelector('.body__wrapp');
+
+const width = window.innerWidth;
 
 const displayCupons = function (cupon) {
   cupon.forEach(({ tilte, description, num, label, image }) => {
@@ -121,7 +130,6 @@ const activeMenuItem = function (e) {
   menuItems.forEach((el) => el.classList.remove("active"));
   e.currentTarget.classList.add("active");
   const text = e.currentTarget.textContent;
-
   headerName.textContent = text;
 };
 
@@ -129,6 +137,41 @@ menuItems.forEach((item) => {
   item.addEventListener("click", activeMenuItem);
 });
 
-mobileBnt.addEventListener('click',function(){
-  console.log('click');
-})
+mobileBnt.addEventListener("click", function () {
+  sidebarMobile.classList.toggle("sidebar__open");
+  this.classList.toggle("close");
+  displayMenuText.forEach((item) => {
+    item.classList.toggle("mobile__text");
+  });
+});
+
+const checkWidth = function () {
+  if (width <= 901) {
+   logoImg.setAttribute('src',`../assets/icons/sidebar/mobile/logo.svg`);
+  }
+};
+checkWidth();
+
+
+let scrollPos = 0;
+
+const checkPosition =function(){
+  let windowY = window.scrollY;
+  if (windowY < scrollPos) {
+    // Scrolling UP
+    headerName.classList.remove('hidden');
+    inputWraper.classList.remove('move__top');
+    headerWraper.classList.remove('srink');
+    bodyWrapp.classList.remove('mtop');
+  } else {
+    // Scrolling DOWN
+    headerName.classList.add('hidden');
+    inputWraper.classList.add('move__top');
+    headerWraper.classList.add('srink');
+    bodyWrapp.classList.add('mtop');
+  }
+  scrollPos = windowY;
+}
+
+window.addEventListener('scroll',checkPosition);
+
